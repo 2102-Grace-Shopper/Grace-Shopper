18a2ab32c9d20d069c43193aa3c6db591ba3c1b1
Cynthia Varelas <cynthiavarelas@Cynthias-iMac.local>
2021-07-15 13:47:05 -0700

    Cvarelas started the DB TABLES

--- db/init_db.js
+++ db/init_db.js
@@ -7,11 +7,85 @@ const {
 async function buildTables() {
   try {
     client.connect();
+      // drop tables in correct order
+    console.log("Starting to Drop Tables")
+    await client.query(`
+    DROP TABLE IF EXISTS reviews;
+    DROP TABLE IF EXISTS order_products;
+    DROP TABLE IF EXISTS orders;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS dog_breed;
+    DROP TABLE IF EXISTS breed;
+    DROP TABLE IF EXISTS dogs;
+    `)
+    console.log('Dropping All Tables')
+       // build tables in correct order
+    console.log("Starting to Build Tables")
+     await client.query(`
+     CREATE TABLE dogs (
+       id SERIAL PRIMARY KEY,
+       name VARCHAR UNIQUE,
+       description VARCHAR(255) NOT NULL,
+       price VARCHAR(255) NOT NULL,
+       age INTEGER  
+     ) 
 
-    // drop tables in correct order
+     CREATE TABLE breed(
+       id SERIAL PRIMARY KEY,
+       breed VARCHAR(255)
+     )
 
-    // build tables in correct order
+     CREATE TABLE dog_breed (
+       id SERIAL PRIMARY KEY,
+       "dogId" INTEGER REFERENCES dogs(id),
+       "breedId" INTEGER REFERENCES breed(id)
+       UNIQUE("dogId", "breedId")
+     )
 
+     CREATE TABLE products (
+       id SERIAL PRIMARY KEY,
+       name VARCHAR(255) NOT NULL,
+       description VARCHAR(255) NOT NULL,
+       price VARCHAR(255) NOT NULL,
+       inStock BOOLEAN DEFAULT false,
+       category VARCHAR(255) NOT NULL
+     )
+
+     CREATE TABLE users(
+       id SERIAL PRIMARY KEY,
+       firstName VARCHAR(255) NOT NULL,
+       lastName VARCHAR(255) NOT NULL,
+       email VARCHAR(255) UNIQUE NOT NULL,
+       username VARCHAR(255) UNIQUE NOT NULL,
+       password VARCHAR(255) UNIQUE NOT NULL,
+       isAdmin BOOLEAN DEFAULT false
+     )
+     
+     CREATE TABLE orders (
+       id SERIAL PRIMARY KEY,
+       status VARCHAR(255) DEFAULT 'created',
+       "userId" INTEGER REFERENCES users(id),
+       "datePlaced" TIMESTAMPZ DEFAULT CURRENT_TIMESTAMP
+     )
+
+     CREATE TABLE order_products (
+       id SERIAL PRIMARY KEY,
+       "productId" INTEGER REFERENCES products(id),
+       "orderId" INTEGER REFERENCES orders(id),
+       price INTEGER NOT NULL,
+       quantity INTEGER NOT NULL DEFAULT (0)
+     )
+     
+     CREATE TABLE reviews (
+       id SERIAL PRIMARY KEY,
+       title VARCHAR(255) NOT NULL,
+       content VARCHAR(255) DEFAULT NULL,
+       "userId" INTEGER REFERENCES users(id),
+       "productId" INTEGER REFERENCES products(id)
+     )
+     `)
+     console.log('Finishing Dropping Tables')
   } catch (error) {
     throw error;
   }
@@ -20,6 +94,7 @@ async function buildTables() {
 async function populateInitialData() {
   try {
     // create useful starting data
+    
   } catch (error) {
     throw error;
   }
