6f16c84e7a939a1b0dbc19a3514f3f672bb7409f
Cynthia Varelas <cynthiavarelas@Cynthias-iMac.local>
2021-07-26 21:06:19 -0700

    Product Routes up

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,13 @@ const productRouter = express.Router();
 
 const {
 getProduct,
-getProductById
+getProductById,
+getOrdersByProducts,
+updateProduct,
+createProduct, 
+destroyProduct
 } = require('../db/index');
 
-
-//This is the GET/product Router:
 productRouter.get('/', async (req, res, next) => {
     try {
         const products = getProduct();
@@ -19,7 +21,6 @@ productRouter.get('/', async (req, res, next) => {
     }
 })
 
-//This is the GET/product/:productId Router
 productRouter.get('/:productId', async(req, res, next) => {
     const { productId } = req.params;
     try {
@@ -31,11 +32,43 @@ productRouter.get('/:productId', async(req, res, next) => {
 })
 
 
+productRouter.get('/:productId/orders', async (req, res, next) => {
+     const { productId } = req.params;
+     try {
+         const orders = await getOrdersByProducts({id: productId});
+         res.send(orders)
+     } catch (error) {
+         next(error)
+     }
+})
 
+productRouter.patch('/:productId', async(req, res, next) => {
+    const { productId } = req.params;
+    try {
+        const updatedProduct = await updateProduct({id: productId, ...req.body})
+        res.send(updatedProduct)
+    } catch (error) {
+        next(error)
+    }
+})
 
+productRouter.post('/', async(req, res, next) => {
+    try {
+        const createdProduct = await createProduct({...req.body})
+        res.send(createdProduct)
+    } catch (error) {
+        next(error)
+    }
+})
 
-
-
-
+productRouter.delete('/:productId', async(req, res, next) => {
+    const { productId } = req.params;
+    try {
+        const deletedProduct = await destroyProduct(productId)
+        res.send(deletedProduct)
+    } catch (error) {
+        next(error)
+    }
+})
 
 module.exports = productRouter;
