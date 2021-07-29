535552cf326ed8fee5825e3f6de37af0fafbf2d4
ChestnutQuezada <chestnutquezada@live.com>
2021-07-28 20:14:41 -0700

    added nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,15 @@
-import React, { useState } from 'react';
-
-// import {
-//   getSomething
-// } from '../api';
-
-import NavBar from './Navbar'
-import Users from './Users'
-import Dogs from './Dogs'
-import Products from './Products'
-//import Order from './Orders'
+import React from 'react';
+import NavReact from './Navbar';
 
 const App = () => {
-  const [products, setProducts] = useState('');
-  const [dogs, setDogs] = useState('');
-  const [users, setUsers] = useState('');
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('');
-  const [token, setToken] = useState(localStorage.getItem('token'))
 
-  // useEffect(() => {
-  //   getSomething()
-  //     .then(response => {
-  //       setMessage(response.message);
-  //     })
-  //     .catch(error => {
-  //       setMessage(error.message);
-  //     });
-  // });
-
-//   return (
-//     <Router>
-//       <Switch>
-//       <div className="App">
-//       <Navbar />
-//       <Route path='/users'>
-//         <Users
-//         users = {users}
-//         setUsers = {setUsers}
-//         />
-//       </Route>
-//       <Route path='/dogs'>
-//         <Dogs
-//         dogs = {dogs}
-//         setDogs = {setDogs}
-//         />
-//       </Route>
-//       <Route path='/products'>
-//         <Products
-//         products = {products}
-//         setProducts = {setProducts}
-//         />
-//       </Route>
-//       <Route path='/registerUserAccount'>
-//         <RegisterForm
-//         username = {username}
-//         setUsername = {setUsername}
-//         password = {password}
-//         setPassword = {setPassword}
-//         />
-//       </Route>
-//       </div>
-//       </Switch>
-//     </Router>
-//   );
+  return (
+    <div>
+      <NavReact>
+        
+      </NavReact>
+    </div>
+  );
 }
 
 
