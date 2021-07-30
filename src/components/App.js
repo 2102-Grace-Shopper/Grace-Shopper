3fa788fff315a3fc23aff0a04476613902f9f156
Cynthia Varelas <cynthiavarelas@Cynthias-iMac.local>
2021-07-30 15:58:59 -0700

    Need To Fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
 import {
-  getSomething
-} from '../api';
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+import NavReact from './Navbar';
+
+import GetAllProducts from './Products';
 
 const App = () => {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    getSomething()
-      .then(response => {
-        setMessage(response.message);
-      })
-      .catch(error => {
-        setMessage(error.message);
-      });
-  });
+  const [products, setProducts] = useState([]);
 
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-      <h2>{ message }</h2>
-    </div>
-  );
+  <Router>
+    <Switch>
+      <div className="App">
+        <NavReact />
+        <Route path='/products'>
+        <GetAllProducts
+        products = {products}
+        setProducts = {setProducts}
+        />
+        </Route>
+      </div>
+    </Switch>
+  </Router>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+
+import React from "react"
+import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavReact = () => {
+    return(
+    <Navbar bg="light" expand="lg">
+    <Container>
+        <Navbar.Brand href="#home">Dog Adoption</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+            <Nav.Link href="#Dogs">Dogs</Nav.Link>
+            <Nav.Link href="#Products">Products</Nav.Link>
+            <NavDropdown title="User" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Cart</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Favorite Dogs</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Login In/Out</NavDropdown.Item>
+            </NavDropdown>
+        </Nav>
+        </Navbar.Collapse>
+    </Container>
+    </Navbar> 
+        )
+}
+
+export default NavReact;
