ca55ecadc7fef7840f902932b75f1cc8dc783162
Cynthia Varelas <cynthiavarelas@Cynthias-iMac.local>
2021-08-04 21:38:07 -0700

    Starting on Cart React

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,3 +1,16 @@
-import React from "react"
+//CART/ORDER REACT UNDER CONSTRUCTION 
+import React, {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
+import { Button } from "reactstrap"
+
+
+const GetAllOrder = (props) => {
+    const {orders, setOrders} = props;
+    const {orderId} = useParams();
+    //UNDER CONSTRUCTION
+}
+
+
+
 
 export default Order
